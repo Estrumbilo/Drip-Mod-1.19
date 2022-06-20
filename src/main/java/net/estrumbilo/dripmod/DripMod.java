a44a79bb016a2572f0b746dfb5cbfb0216a1bdd9
Estrumbilo <edua.silva05@gmail.com>
2022-06-20 21:04:26 +0100

    Initial commit

---
+++ src/main/java/net/estrumbilo/dripmod/DripMod.java
@@ -0,0 +1,15 @@
+package net.estrumbilo.dripmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DripMod implements ModInitializer {
+	public static final String MOD_ID = "dripmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/estrumbilo/dripmod/DripModClient.java
@@ -0,0 +1,10 @@
+package net.estrumbilo.dripmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DripModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
